be570562beea38255657f2cabf4fbc9c410efb01
shakila <skshakilask67@gmail.com>
2025-11-25 22:11:38 +0530

    fix: added  envs for prod , dev

--- src/config/config.cjs
+++ src/config/config.cjs
@@ -1,28 +1,33 @@
-require("dotenv").config();
+require("dotenv").config({
+  path: process.env.NODE_ENV === "production" ? ".env.production" : ".env",
+});
 
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: process.env.DB_USER || "postgres",
+    password: process.env.DB_PASS || "shammu",
+    database: process.env.DB_NAME || "ecommerce_db",
+    host: process.env.DB_HOST || "127.0.0.1",
     port: process.env.DB_PORT || 5432,
     dialect: "postgres",
   },
   test: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME_TEST,
-    host: process.env.DB_HOST,
+    username: process.env.DB_USER || "postgres",
+    password: process.env.DB_PASS || "shammu",
+    database: process.env.DB_NAME_TEST || "ecommerce_db_test",
+    host: process.env.DB_HOST || "127.0.0.1",
     port: process.env.DB_PORT || 5432,
     dialect: "postgres",
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5432,
+    url: process.env.DATABASE_URL, // Render Postgres URL
     dialect: "postgres",
-  }
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // required for Render
+      },
+    },
+    logging: false,
+  },
 };
